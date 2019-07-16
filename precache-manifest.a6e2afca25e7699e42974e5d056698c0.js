1d5ae5f3a1712878b6ce83a888a98ba25b448da3
WeiTangLau <lauweitang@gmail.com>
2019-07-16 14:10:17 +0800

    Updates

---
+++ precache-manifest.a6e2afca25e7699e42974e5d056698c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256a7cc0a58379698ef50f8d711363ca",
+    "url": "/InventoryTrackingApp/index.html"
+  },
+  {
+    "revision": "10343249c14a9ff75c60",
+    "url": "/InventoryTrackingApp/static/css/2.00da11b9.chunk.css"
+  },
+  {
+    "revision": "10343249c14a9ff75c60",
+    "url": "/InventoryTrackingApp/static/js/2.a0d969bc.chunk.js"
+  },
+  {
+    "revision": "fa6cca07e02a440945bb",
+    "url": "/InventoryTrackingApp/static/js/main.074dc419.chunk.js"
+  },
+  {
+    "revision": "9fbbcb11ad85aad6ea67",
+    "url": "/InventoryTrackingApp/static/js/runtime~main.c44761cc.js"
+  }
+]);
